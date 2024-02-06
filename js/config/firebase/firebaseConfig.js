16bf95d15217e963bb2a41be5aa57fac4eb48db8
Jack Hewertson <122547280+JackHewert@users.noreply.github.com>
2024-02-06 16:56:00 -0500

    first

---
+++ js/config/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "your key",
+	authDomain: "your key",
+	projectId: "your key",
+	storageBucket: "your key",
+	messagingSenderId: "your key",
+	appId: "your key",
+};
+
+export { firebaseConfig };
