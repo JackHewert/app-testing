d7c4845db66e006da5ed97c2f18d806fa458cf6a
Jack Hewertson <122547280+JackHewert@users.noreply.github.com>
2024-02-09 15:11:00 -0500

    1

---
+++ js/lib/firebase/config/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyB96jYpw6ukPaQeFiDD9BGCrcMMyDztbDU",
+	authDomain: "test-751b7.firebaseapp.com",
+	databaseURL: "https://test-751b7-default-rtdb.firebaseio.com",
+	projectId: "test-751b7",
+	storageBucket: "test-751b7.appspot.com",
+	messagingSenderId: "588483897354",
+	appId: "1:588483897354:web:05ab180e9c560949f097f3",
+	measurementId: "G-25VR5Y2QHC"
+};
+
+export { firebaseConfig };
+
---
+++ js/lib/firebase/config/firebase/firebaseInit.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+import { firebaseConfig } from './firebaseConfig';
+
+// Initialize Firebase connects to the project we setup
+// connect to the app... created
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Realtime Database and get a reference to the service
+// connecting  to the RTDB   ---> ref
+const db = getDatabase(app);
+
+export { db };
