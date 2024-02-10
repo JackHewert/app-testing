7273a865f5585e36627422c35257b1c05941f98f
Jack Hewertson <122547280+JackHewert@users.noreply.github.com>
2024-02-10 11:56:43 -0500

    0.02 reset 1

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-	import { resolve } from 'path';
-	import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-	// https://vitejs.dev/config/
-	export default defineConfig({
-		server: {
-			port: 3000,
+// https://vitejs.dev/config/
+export default defineConfig({
+	server: {
+		port: 3000,
+	},
+	resolve: {
+		alias: {
+			'@': './src',
 		},
-		resolve: {
-			alias: {
-				'@': './src',
-			},
-		},
+	},
+	build: {
 		build: {
+			rollupOptions: {
+				external: ['js/tag.js']
+			},
 			rollupOptions: {
 				input: {
 					index: resolve(__dirname, './index.html'),
@@ -20,7 +24,8 @@
 				},
 			},
 		},
-	});
+	}
+});
 
 /* 
 
